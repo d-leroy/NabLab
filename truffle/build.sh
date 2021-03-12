9bb2a7ee94593179b8d9d16db1f376a91aa5f2c3
LEROY Dorian <dorian.leroy@irisa.fr>
2021-03-12 10:49:33 +0100

    0.4.0

--- truffle/build.sh
+++ truffle/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mvn install:install-file    -Dfile=../plugins/fr.cea.nabla/target/fr.cea.nabla-0.3.3-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla    -Dversion=0.3.3-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
-mvn install:install-file    -Dfile=../plugins/fr.cea.nabla.ir/target/fr.cea.nabla.ir-0.3.3-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.ir    -Dversion=0.3.3-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=../plugins/fr.cea.nabla/target/fr.cea.nabla-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=../plugins/fr.cea.nabla.ir/target/fr.cea.nabla.ir-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.ir    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=../plugins/fr.cea.nabla.javalib/target/fr.cea.nabla.javalib-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.javalib    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
 mvn package
 
